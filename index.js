42f1d733286f69373a5032ef636e69769a83699b
Yohan <yohan-fmr@hotmail.com>
2024-06-16 16:59:00 +0200

    connect triangle to button validate

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function drawTriangle(numLines){
   let strTriangle = "";
   let SYMBOLE = "*";
-  for (let i=0; i <= numLines; i++)
-  {
+  for (let i = 0; i <= numLines; i++) {
     strTriangle += SYMBOLE.repeat(i) + "\n";
   }
   return strTriangle;
 }
+
+// Événement click pour le bouton
+document.getElementById("validateButton").addEventListener("click", function() {  
+  // Dessiner le triangle
+  const triangle = drawTriangle(3);
+      
+  // Afficher le triangle dans l'élément HTML
+  document.getElementById("triangleZone").innerText = triangle;
+});
